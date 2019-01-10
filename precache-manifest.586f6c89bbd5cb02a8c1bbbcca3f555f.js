1c9bd228cb6c955dcbc403a08318c389f4b2bd6c
umutsevinc <umut.sevinc@hotmail.fr>
2019-01-10 23:39:39 +0100

    Updates

---
+++ precache-manifest.586f6c89bbd5cb02a8c1bbbcca3f555f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1449a9097811eb590f97",
+    "url": "/umutsevinc.github.io/static/js/runtime~main.1449a909.js"
+  },
+  {
+    "revision": "9ab9d72a4c311f00a41b",
+    "url": "/umutsevinc.github.io/static/js/main.9ab9d72a.chunk.js"
+  },
+  {
+    "revision": "38fd6c207d3d56d4920a",
+    "url": "/umutsevinc.github.io/static/js/1.38fd6c20.chunk.js"
+  },
+  {
+    "revision": "9ab9d72a4c311f00a41b",
+    "url": "/umutsevinc.github.io/static/css/main.573bd2db.chunk.css"
+  },
+  {
+    "revision": "38fd6c207d3d56d4920a",
+    "url": "/umutsevinc.github.io/static/css/1.721c6636.chunk.css"
+  },
+  {
+    "revision": "ea69a0a76cbe7202f7d7dfd74d13e4ac",
+    "url": "/umutsevinc.github.io/index.html"
+  }
+];
